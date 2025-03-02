cb9f75457e996f49402b166e62a8c8568c48cc42
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-03-02 17:58:15 -0300

    Quinto exercício

---
+++ Exercicios_CursoEmVideo/ex_05.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um número inteiro: '))
+
+print(f'O número escolhido foi {n}, seu antecessor {n - 1}, seu sucessor {n + 1}')
