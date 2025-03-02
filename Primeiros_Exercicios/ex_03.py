431fcdc740544a53ad8269e8576f7b9a03e05961
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-03-02 17:44:37 -0300

    Terceiro exercício

---
+++ Primeiros_Exercicios/ex_03.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+soma = (n1 + n2)
+
+print(f'{n1} + {n2} = {soma}')
