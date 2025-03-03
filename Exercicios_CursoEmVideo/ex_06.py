f4678374feee4b351b665d7fd34a495ff36b113f
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-03-03 19:27:18 -0300

    Exercícios Curso Em Vídeo

---
+++ Exercicios_CursoEmVideo/ex_06.py
@@ -0,0 +1,5 @@
+#Crie um Algoritmo que leia um número e mostre o seu dobro, triplo, raiz quadrada.
+
+n = int(input('Digite um número: '))
+
+print(f'O número escolhido {n}, seu dobro {n * 2}, se triplo {n * 3} e a raíz quadrada {n ** (1/2)}')
