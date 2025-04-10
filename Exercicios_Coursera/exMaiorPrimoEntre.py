439a827673e63e83b709cfd9c059cbd5c4189a92
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-04-10 06:50:15 -0300

    MaiorPrimoEntre

--- Exercicios_Coursera/exMaiorPrimoEntre.py
+++ Exercicios_Coursera/exMaiorPrimoEntre.py
@@ -1,15 +1,18 @@
-
-
-n = int(input('Digite um número inteiro: '))
-contador = n
-primo = 0
-maior_primo = 0
-
+#Escreva a função maior_primo que recebe um número inteiro maior ou igual a 2 como 
+#parâmetro e devolve o maior número primo menor ou igual ao número passado à função
 def maior_primo(n):
-    while contador != 0:
-        if (n % 1 == 0):
-            primo += 1
-        if (n % contador == 0):
-            primo += 1
-        if ()
-        contador - 1
+    primo = 0
+
+    while primo == 0:
+
+        if (n % 2) == 0 or (n % 3) == 0:
+            n -= 1
+        elif (n > 1) and (n % 2) != 0 and (n % 3) != 0:
+            primo = n
+        else:
+            return 'Número digitado inválido'
+
+    return primo
+
+maior_primo(100)
+maior_primo(7)
