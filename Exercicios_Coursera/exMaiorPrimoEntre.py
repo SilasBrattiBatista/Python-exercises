a31b19914f984a5ea5f9146906a2fbdcd14e68c4
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-04-10 20:57:16 -0300

    MaiorPrimo

--- Exercicios_Coursera/exMaiorPrimoEntre.py
+++ Exercicios_Coursera/exMaiorPrimoEntre.py
@@ -1,16 +1,22 @@
 #Escreva a função maior_primo que recebe um número inteiro maior ou igual a 2 como 
 #parâmetro e devolve o maior número primo menor ou igual ao número passado à função
+
+
 def maior_primo(n):
-    primo = 0
-    divisor = n
-    ePrimo = 0
+    while n >= 2:
+        divisor = 1
+        cont = 0
 
-    while primo == 0:
+        while divisor <= n:
+            if n % divisor == 0:
+                cont += 1
+            divisor += 1
 
-        if (n % divisor) == 0:
-            ePrimo += 1
-        
-        divisor -= 1
+        if cont == 2:
+            return n
+        n -= 1
 
-maior_primo(100)
-maior_primo(7)
+
+
+print(maior_primo(100))
+print(maior_primo(7))
