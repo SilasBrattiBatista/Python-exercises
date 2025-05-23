18cd1529a7cb1979f4a09e1710ffa84671c1dbbe
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-05-22 23:28:37 -0300

    Update ex_23.py

--- Exercicios_CursoEmVideo/ex_23.py
+++ Exercicios_CursoEmVideo/ex_23.py
@@ -1,3 +1,5 @@
 #Faça um programa que leia um número de 0 a 9999 e mostre
 #na tela um dos digitos separados
 #Ex: 1834: 4 unidade, 3 dezena, 8 centena, 1 milhar
+
+n = int(input("Digite um número: "))
