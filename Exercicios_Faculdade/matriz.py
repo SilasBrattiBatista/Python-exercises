e8c5c194caacc61a4eff156468e37fc81dda273b
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-06-18 23:38:10 -0300

    Update matriz.py

--- Exercicios_Faculdade/matriz.py
+++ Exercicios_Faculdade/matriz.py
@@ -3,12 +3,18 @@
 #Calcule a soma dos elementos da diagonal principal da matriz
 
 n = int(input("Digite o tamanho da matriz "))
-contador1 = 0
-contador2 = 0
+contador1 = []
+contador2 = []
+resultado = 0
 
 while contador1 < n:
     print()
+    
     contador1 += 1
     while contador2 < n:
         print(contador1, contador2)
+        if contador1 == contador2:
+            resultado = contador1 + contador2
+        
         contador2 += 1
+        
