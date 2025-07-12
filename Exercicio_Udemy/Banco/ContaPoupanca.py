b92a10b2a2c83aeea13eb94b166aef4c973df289
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-07-12 19:56:18 -0300

    SistemaBancario

--- Exercicio_Udemy/Banco/ContaPoupanca.py
+++ Exercicio_Udemy/Banco/ContaPoupanca.py
@@ -3,4 +3,8 @@ import Conta
 class ContaPoupanca(Conta):
     
     def sacar(self, valor):
-        self.saldo -= valor
+        
+        if (self.saldo - valor) < 0:
+            print("Valor insuficiente para saque")
+        else:
+            self.saldo -= valor
