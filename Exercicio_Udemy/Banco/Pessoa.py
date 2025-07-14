f4da12405b403ece9dd921e7153972f380a68fa1
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-07-14 20:29:23 -0300

    Update Pessoa.py

--- Exercicio_Udemy/Banco/Pessoa.py
+++ Exercicio_Udemy/Banco/Pessoa.py
@@ -1,11 +1,23 @@
-import abc
-
-
-class Pessoa(abc.ABC):
-    
-    def __init__(self, nome: str, idade: int, cpf: str):
+class Pessoa:
+    def __init__(self, nome: str, idade: int):
         self.nome = nome
         self.idade = idade
-        self.cpf = cpf
+        
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome: str):
+        self._nome = nome
+    
+    @property
+    def idade(self):
+        return self._idade
+    
+    @idade.setter
+    def idade(self, idade: int):
+        self._idade = idade
+    
         
     
