e66a977e3795f0dccf362255007319acf0a87bf8
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-07-14 19:39:07 -0300

    Update Conta.py

--- Exercicio_Udemy/Banco/Conta.py
+++ Exercicio_Udemy/Banco/Conta.py
@@ -18,3 +18,12 @@ class Conta:
         
     def detalhes(self, msg=''):
         print(f"O saldo atual é {self.saldo} {msg}")
+        
+class ContaPoupanca(Conta):
+    
+    def sacar(self, valor):
+        
+        if (self.saldo - valor) < 0:
+            print("Valor insuficiente para saque")
+        else:
+            self.saldo -= valor
