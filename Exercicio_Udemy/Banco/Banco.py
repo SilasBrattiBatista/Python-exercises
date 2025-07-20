15701c4f70670a51cedbee1933ea684c45cbe134
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2025-07-20 18:28:25 -0300

    Create Banco.py

---
+++ Exercicio_Udemy/Banco/Banco.py
@@ -0,0 +1,10 @@
+import Cliente
+import Conta
+
+class Banco:
+    
+    def __init__(self, conta: Conta.Conta, cliente: Cliente.Cliente) -> None:
+        self.conta: Conta.Conta = conta
+        self.cliente: Cliente.Cliente = cliente
+        
+    
